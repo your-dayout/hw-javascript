0baa7a608e1be616842ff9638107e1bd5294e72b
Nadiia Yurieva <yurievanadiia@gmail.com>
2024-09-30 22:29:31 +0100

    discount applied

--- cart_summ.js
+++ cart_summ.js
@@ -3,7 +3,8 @@ const cart = [
     {name: "Milk", price: 2, quantity: 1},
     {name: "Bread", price: 1, quantity: 1},
     {name: "Butter", price: 4, quantity: 1},
-    {name: "Eggs", price: 8, quantity:2}
+    {name: "Eggs", price: 8, quantity:2},
+    {name: "Engagement ring", price: 1000, quantity:1}
 ];
 function CartTotal (cart) {
     let total=0;
@@ -11,7 +12,9 @@ function CartTotal (cart) {
         const item = cart[i];
     total += (item.price*item.quantity);
     }
-
+if (total>100){
+    total=total*0.9;
+}
     return total;
 
 }
