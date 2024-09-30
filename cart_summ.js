c29cb5a3c12cd818ec999ebd4908711f6c3ae9a2
Nadiia Yurieva <yurievanadiia@gmail.com>
2024-09-30 22:07:05 +0100

    fixed most of bugs

--- cart_summ.js
+++ cart_summ.js
@@ -1,16 +1,16 @@
 const cart = [
-    {name: "Salami", price: 8, quantity: 1}
-    {name: "Milk", price: 2, quantity: 1}
-    {name: "Bread", price: 1, quantity: 1}
-    {name: "Butter", price: 4, quantity: 1}
+    {name: "Salami", price: 8, quantity: 1},
+    {name: "Milk", price: 2, quantity: 1},
+    {name: "Bread", price: 1, quantity: 1},
+    {name: "Butter", price: 4, quantity: 1},
     {name: "Eggs", price: 8, quantity:2}
-]
+];
 function CartTotal (cart) {
     let CartTotal=0;
-    for (let=i; i<cart.length; i++)
+    for (let i=0; i<cart.length; i++){
         const item = cart[i];
-    total = total +(item.price*item.quantity);
+    total = CartTotal +(item.price*item.quantity);
 
-    return total;
+    return CartTotal;
 
 }
