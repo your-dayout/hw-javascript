a21631c03a98a26fa62e25cd1259ad074346eb9b
Nadiia Yurieva <yurievanadiia@gmail.com>
2024-10-14 05:30:58 +0100

    Library class

--- library.js
+++ library.js
@@ -3,7 +3,7 @@ class book {
     #author;
     #availability;
 }
-constructor(title; author) {
+constructor(title, author) {
     this.#title=title;
     this.#author=author;
     this.#availability=true;
@@ -18,10 +18,41 @@ constructor(title; author) {
         return this.#availability;
     }
     CheckoutBook() {
-        if (this.#available) {
-            this.#available = false;
+        if (this.#availability) {
+            this.#availability = false;
         } else {
             console.log ("Sorry "${this.#tytle" is not available."});
         }
+    }
+}
+class Library {
+    constructor() {
+        this.books = [];
+        } 
+    addBook(book){
+        this.books.push(book);
+    }
+    CheckoutBook(title) {
+        const book = this.books.find(book => book.getTitle() === title);
+        if (book) {
+        if (book.getAvailability()) {
+            book.CheckoutBook();
+            console.log("You have checked out "${book.getTitle()}" by ${book.getAuthor()}.");
+        }
+    } else {
+        console.log("Sorry, "${title}" is currently not available.");
+    { else {
+    console.log("Book titled "${title}" not found in the library.");
+    }
+}
+viewAvailableBooks() {
+    const availableBooks = this.books.filter(book => book.getAvailability());
+    if (availableBooks.length > 0) {
+        console.log("Available books:");
+        availableBooks.forEach(book => {
+            console.log("${book.getTitle()}" by ${book.getAuthor()}`);
+        });
+        } else {
+         console.log("No books are currently available.");
     }
 }
